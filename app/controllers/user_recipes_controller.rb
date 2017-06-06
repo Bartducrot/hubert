c79cd1a475cf1052f8fc07848b008dc5178fd6f7
Barthelemy Ducrot <barthelemy.ducrot@hei.fr>
2017-06-06 15:48:58 +0100

    add some route, define a mechanism for the calendar, add a basic skeletton for the user view

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -1,15 +1,35 @@
 class UserRecipesController < ApplicationController
-  def new
-    @user_recipe = UserRecipe.new()
+
+  def index
+    @day = Date.parse(params[:date])
+    @user_recipe = UserRecipe.new
+    if params[:date].present?
+      @user_recipes = UserRecipe.where(date: params[:date]).where(user: current_user)
+    else
+      @user_recipes = UserRecipe.where(date: Date.today).where(user: current_user)
+    end
   end
 
   def create
+    @user_recipe = UserRecipe.new(user_recipe_params)
+    @user_recipe.save
+    redirect_to user_user_recipes_path(current_user)
   end
 
   def edit
   end
 
   def update
+  end
+
+  def date
+
+  end
+
+  private
+
+  def user_recipe_params
+    params.require(:user_recipe).permit(:number_of_people, :recipe_id, :user_id, :date)
   end
 
 end
