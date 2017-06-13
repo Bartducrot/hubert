9be90281527b222cbd720c9d1cad5ab2b077804d
Barthelemy Ducrot <barthelemy.ducrot@hei.fr>
2017-06-13 18:51:56 +0100

    failed to use tor, add a wait method between each seed

--- Gemfile
+++ Gemfile
@@ -23,12 +23,15 @@ gem 'jquery-ui-rails'
 
 
 #seed gem
-gem 'nokogiri' ## Parse HTML
-gem 'watir' ## Create a browser which would be dummy
-gem 'tor'
-gem 'tor_requests'
-gem 'selenium' ## Web driver
-gem 'aws-sdk', '-> 2.3' ## Amazon cloud
+# gem 'nokogiri' ## Parse HTML
+# gem 'watir' ## Create a browser which would be dummy
+# gem 'tor'
+# gem 'tor_requests'
+# gem 'selenium' ## Web driver
+# gem 'selenium-webdriver', '~> 3.4', '>= 3.4.1'
+# gem 'aws-sdk', '~> 2.3' ## Amazon cloud
+# gem 'paperclip'
+# gem 'phantomjs', '~> 2.1', '>= 2.1.1.0'
 
 gem 'ingreedy', '~> 0.0.9'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,40 @@
 require 'nokogiri'
 require 'open-uri'
 require 'date'
+# require 'watir'
+# require 'selenium-webdriver'
+# require 'selenium'
 
-# puts "Start seeding ingredients"
-# INGREDIENTS = {
-#   "vegetable" => ["garlic", "onion", "olive", "tomato", "potato", "salad greens",
-#     "carrot", "basil", "parsley", "rosemary", "bell pepper", "chili pepper", "corn",
-#     "ginger", "mushroom", "broccoli", "spinach", "green beans", "celery", "red onion",
-#     "cilantro", "cucumber", "pickle", "dill", "avocado", "sweet potato", "zucchini",
-#     "shallot", "cabbage", "asparagus", "cauliflower", "mint", "pumpkin", "kale", "scallion",
-#     "squash", "sun dried tomato", "horseradish", "sweet corn", "beet"],
-#   "dairy" => ["butter", "eggs", "milk", "parmesan", "cheddar", "cream", "sour cream", "cream cheese",
-#     "mozzarella", "american cheese", "yogurt", "evaporated milk", "condensed milk", "whipped cream",
-#     "half and half""monterey jack cheese", "feta", "cottage cheese", "ice cream", "goat cheese",
-#     "frosting", "swiss cheese", "buttermilk", "velveeta", "ricotta", "powdered milk", "blue cheese",
-#     "provolone", "colby cheese", "gouda", "pepper jack", "italian cheese", "soft cheese", "romano",
-#     "brie", "pepperjack cheese", "custard", "cheese soup", "pizza cheese", "ghee", "pecorino cheese",
-#     "gruyere", "creme fraiche", "neufchatel", "muenster", "asiago", "queso fresco cheese", "hard cheese",
-#      "havarti cheese", "mascarpone"],
-#   "meat" => ["chicken breast", "ground beef", "bacon", "sausage", "cooked chicken",
-#    "ham", "veal", "beef steak", "hot dog", "pork chops", "chicken thighs",
-#    "ground turkey", "pork", "turkey", "pepperoni", "whole chicken", "chicken leg",
-#     "ground pork", "chicken wings", "chorizo", "polish sausage", "salami", "pork roast",
-#     "ground chicken", "pork ribs", "venison", "spam", "lamb", "pork shoulder",
-#     "beef roast", "bratwurst", "prosciutto", "chicken roast", "bologna", "corned beef",
-#     "lamb chops", "ground lamb", "beef ribs", "duck", "pancetta", "beef liver",
-#     "leg of lamb", "chicken giblets", "beef shank", "pork belly", "cornish hen",
-#     "lamb shoulder", "lamb shank"],
-#   "sauce" => ["mustard", "ketchup"]
-# }
+
+# browser = Watir::Browser.new :phantomjs
+#   profile = Selenium::WebDriver::PhantomJS::Profile.new
+#   profile['network.proxy.socks'] = "127.0.0.1"
+#   profile['network.proxy.socks_port'] = 9050
+#   profile['network.proxy.type'] = 1
+#   browser = Watir::Browser.new :phantomjs, :profile => profile
+
+#   puts "hello there!"
+#   browser.goto "www.hubert-cooks.com"
+#   # p browser.public_methods.sort
+#   browser.goto("http://www.whatsmyip.org/")
+#   ip = browser.span(:id, "ip").text
+#   puts ip
+#   browser.goto "http://allrecipes.com/recipe/15000"
+#   title_allrecipe = browser.title
+#   puts title_allrecipe
+# browser.close
 
 
 
-ShoppingItem.destroy_all
-UserRecipe.destroy_all
-# RecipeIngredient.destroy_all
+# ShoppingItem.destroy_all
+# UserRecipe.destroy_all
+# # RecipeIngredient.destroy_all
 # Recipe.destroy_all
 # Ingredient.destroy_all
 
 
 100.times do |i|
+
     base_url = "http://allrecipes.com/recipe/"
     # index_start = 6663
     index_start = 15000
@@ -149,3 +144,32 @@ UserRecipe.destroy_all
 
 
 end
+
+
+# puts "Start seeding ingredients"
+# INGREDIENTS = {
+#   "vegetable" => ["garlic", "onion", "olive", "tomato", "potato", "salad greens",
+#     "carrot", "basil", "parsley", "rosemary", "bell pepper", "chili pepper", "corn",
+#     "ginger", "mushroom", "broccoli", "spinach", "green beans", "celery", "red onion",
+#     "cilantro", "cucumber", "pickle", "dill", "avocado", "sweet potato", "zucchini",
+#     "shallot", "cabbage", "asparagus", "cauliflower", "mint", "pumpkin", "kale", "scallion",
+#     "squash", "sun dried tomato", "horseradish", "sweet corn", "beet"],
+#   "dairy" => ["butter", "eggs", "milk", "parmesan", "cheddar", "cream", "sour cream", "cream cheese",
+#     "mozzarella", "american cheese", "yogurt", "evaporated milk", "condensed milk", "whipped cream",
+#     "half and half""monterey jack cheese", "feta", "cottage cheese", "ice cream", "goat cheese",
+#     "frosting", "swiss cheese", "buttermilk", "velveeta", "ricotta", "powdered milk", "blue cheese",
+#     "provolone", "colby cheese", "gouda", "pepper jack", "italian cheese", "soft cheese", "romano",
+#     "brie", "pepperjack cheese", "custard", "cheese soup", "pizza cheese", "ghee", "pecorino cheese",
+#     "gruyere", "creme fraiche", "neufchatel", "muenster", "asiago", "queso fresco cheese", "hard cheese",
+#      "havarti cheese", "mascarpone"],
+#   "meat" => ["chicken breast", "ground beef", "bacon", "sausage", "cooked chicken",
+#    "ham", "veal", "beef steak", "hot dog", "pork chops", "chicken thighs",
+#    "ground turkey", "pork", "turkey", "pepperoni", "whole chicken", "chicken leg",
+#     "ground pork", "chicken wings", "chorizo", "polish sausage", "salami", "pork roast",
+#     "ground chicken", "pork ribs", "venison", "spam", "lamb", "pork shoulder",
+#     "beef roast", "bratwurst", "prosciutto", "chicken roast", "bologna", "corned beef",
+#     "lamb chops", "ground lamb", "beef ribs", "duck", "pancetta", "beef liver",
+#     "leg of lamb", "chicken giblets", "beef shank", "pork belly", "cornish hen",
+#     "lamb shoulder", "lamb shank"],
+#   "sauce" => ["mustard", "ketchup"]
+# }
