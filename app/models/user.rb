f94b12e3e3293263b5b2573484a8c19c51507e4f
Barthelemy Ducrot <barthelemy.ducrot@hei.fr>
2017-06-14 11:14:28 +0100

    change <<<master on user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,10 +47,6 @@ class User < ApplicationRecord
     return user_liked_recipes
   end
 
-
-
-
->>>>>>> master
   def self.find_for_facebook_oauth(auth)
     user_params = auth.slice(:provider, :uid)
     user_params.merge! auth.info.slice(:email, :first_name, :last_name)
