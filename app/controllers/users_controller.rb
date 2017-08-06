8c1284f5694f06210eaac5abd61b3d8f06d9b9ec
Barthelemy Ducrot <barthelemy.ducrot@hei.fr>
2017-08-07 01:28:40 +0200

    add some comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,14 +23,18 @@ class UsersController < ApplicationController
   end
 
   def set_user_to_vegetarian
+    # take the current user
     user = current_user
+    # Iterate one each Ingredient with meat category
     Ingredient.all.where(category: "meat").each do |ingr|
       taste = IngredientTaste.find_by(user: user, ingredient: ingr)
+      # check if there is already a define taste on it, puts it to false
       if taste
         taste.like = false
         taste.save
         puts "as #{ingr.name} is in the meat category, the vegetarian don't like this => #{user.first_name} don't like this anymore"
       else
+        # if no taste is define for this ingredient, create it and puts "false" as the value for "like"
         taste = IngredientTaste.create(user: user, ingredient: ingr, like: false)
         puts "as #{ingr.name} is in the meat category, the vegetarian don't like this => #{user.first_name} don't like this"
       end
